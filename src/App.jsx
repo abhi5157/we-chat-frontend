b59f4ea62bf86760bc110d11dbe2f37de77be03e
Abhishek <abhishekjoshi035@.com>
2024-10-30 12:45:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Signup from "./components/Login/Signup/Signup";
+import Forgot from "./components/Login/Forgot/Forgot";
+import Login from "./components/Login/Login/Login";
+import ChatApp from "./components/Chat-Sys/ChatApp";
+import SetPasswordPage from "./components/Login/SetPasswordPage";
+import VerifyPage from "./components/Login/VerifyCode";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
+// import AccountProvider from "./context/AccountProvider";
+
+function App() {
+  const user = localStorage.getItem("token");
+  // const clientId =
+  //   "162265749756-6rogh84nkiukqh6ntlvhunm0oaqb3k6r.apps.googleusercontent.com";
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {user && <Route path="/" exact element={<ChatApp />} />}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgotpassword" element={<Forgot />} />
+        <Route path="/" element={<Navigate replace to="/login" />} />
+
+        <Route path="/app" element={<ChatApp />} />
+        <Route path="/reset" element={<SetPasswordPage />} />
+        <Route path="/verify" element={<VerifyPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Chat-Sys/Header.jsx
@@ -0,0 +1,177 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Avatar, IconButton, Menu, MenuItem, Dialog, Box } from "@mui/material";
+import EditProfile from "./EditProfile";
+import profile from "../../assets/profile.png";
+import { ChatIcon, ContactsIcon, NotificationIcon } from "./Icons";
+import axios from "axios";
+
+const Header = ({
+  onEditProfile,
+  onToggleSidebar,
+  activeSidebar,
+  onLogout,
+}) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [openEditProfile, setOpenEditProfile] = useState(false);
+  const [userData, setUserData] = useState({});
+  const [avatarPreview, setAvatarPreview] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          "http://http://autorack.proxy.rlwy.net:12882/api/users/me",
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        const userData = response.data;
+        setUserData(response.data);
+        if (userData.avatar) {
+          setAvatarPreview(
+            `http://http://autorack.proxy.rlwy.net:12882/${userData.avatar}`
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const handleProfileClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleEditProfile = () => {
+    handleClose();
+    onEditProfile();
+  };
+
+  const handleLogout = () => {
+    handleClose();
+    localStorage.removeItem("token");
+    onLogout();
+    navigate("/login");
+  };
+
+  const iconButtonClass =
+    "p-2 rounded-full transition-colors bg-white text-gray-600 hover:bg-gray-200";
+  const activeIconButtonClass =
+    "p-2 rounded-full transition-colors bg-[#E0F2F1] text-gray-800";
+
+  return (
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto px-4 py-2 flex items-center justify-between flex-wrap md:flex-nowrap">
+        {/* Left Section - Logo and Icons */}
+        <Box className="flex items-center space-x-2 md:space-x-5 mb-2 md:mb-0">
+          <h1 className="text-xl md:text-2xl font-bold text-gray-800">
+            WECHAT
+          </h1>
+          <div className="flex space-x-1">
+            <IconButton
+              className={
+                activeSidebar === "chat"
+                  ? activeIconButtonClass
+                  : iconButtonClass
+              }
+              onClick={() => onToggleSidebar("chat")}
+            >
+              <ChatIcon />
+            </IconButton>
+            {/* <IconButton
+              className={
+                activeSidebar === "contacts"
+                  ? activeIconButtonClass
+                  : iconButtonClass
+              }
+              onClick={() => onToggleSidebar("contacts")}
+            >
+              <ContactsIcon />
+            </IconButton> */}
+          </div>
+        </Box>
+
+        {/* Right Section - Notifications and Profile */}
+        <div className="flex items-center space-x-2 md:space-x-4">
+          {/* <IconButton className={iconButtonClass}>
+            <NotificationIcon />
+          </IconButton> */}
+          <IconButton onClick={handleProfileClick}>
+            <Avatar src={avatarPreview} alt="Profile" />
+          </IconButton>
+        </div>
+      </div>
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+        <MenuItem onClick={handleEditProfile}>Edit Profile</MenuItem>
+        <MenuItem onClick={handleLogout}>Log Out</MenuItem>
+      </Menu>
+      <Dialog open={openEditProfile} onClose={() => setOpenEditProfile(false)}>
+        <EditProfile onClose={() => setOpenEditProfile(false)} />
+      </Dialog>
+    </header>
+  );
+};
+
+export default Header;
+// import React from "react";
+// import { Avatar, IconButton } from "@mui/material";
+// import {
+//   ChatIcon,
+//   ContactsIcon,
+//   SearchIcon,
+//   AddIcon,
+//   NotificationIcon,
+// } from "./Icons";
+
+// const Header = ({ onEditProfile, onToggleSidebar, activeSidebar }) => {
+//   const iconButtonClass =
+//     "p-2 rounded-full transition-colors bg-white text-gray-600 hover:bg-gray-200";
+//   const activeIconButtonClass =
+//     "p-2 rounded-full transition-colors bg-[#E0F2F1] text-gray-800";
+
+//   return (
+//     <header className="bg-white shadow-md">
+//       <div className="container mx-auto px-4 py-2 flex items-center justify-between">
+//         <h1 className="text-2xl font-bold text-gray-800">WECHAT</h1>
+//         <div className="flex items-center space-x-4">
+//           <IconButton
+//             className={
+//               activeSidebar === "chat" ? activeIconButtonClass : iconButtonClass
+//             }
+//             onClick={() => onToggleSidebar("chat")}
+//           >
+//             <ChatIcon />
+//           </IconButton>
+//           <IconButton
+//             className={
+//               activeSidebar === "contacts"
+//                 ? activeIconButtonClass
+//                 : iconButtonClass
+//             }
+//             onClick={() => onToggleSidebar("contacts")}
+//           >
+//             <ContactsIcon />
+//           </IconButton>
+
+//           <IconButton className={iconButtonClass}>
+//             <NotificationIcon />
+//           </IconButton>
+//           <IconButton onClick={onEditProfile}>
+//             <Avatar src="/api/placeholder/32/32" alt="Profile" />
+//           </IconButton>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
